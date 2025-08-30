09f32b2bd8395416c52ad9b02f25d84c9a45d574
00043963 <00043963@randon.com.br>
2025-08-30 17:59:09 -0300

    feat: initial commit

---
+++ api_people/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
+
+class Endereco(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='enderecos')
+    rua = models.CharField(max_length=200)
+    numero = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=2)
+    cep = models.CharField(max_length=9)
+
+    def __str__(self):
+        return f"{self.rua}, {self.numero} - {self.cidade}/{self.estado}"
