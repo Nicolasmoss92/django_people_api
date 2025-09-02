b1dcd817190e6adf5138428f05ab13d692dacb03
00043963 <00043963@randon.com.br>
2025-09-02 16:27:37 -0300

    feat: black

--- api_people/migrations/0001_initial.py
+++ api_people/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name="Usuario",
+            name="User",
             fields=[
                 (
                     "id",
@@ -23,23 +23,38 @@ class Migration(migrations.Migration):
                         verbose_name="ID",
                     ),
                 ),
-                ("nome", models.CharField(max_length=100)),
-                ("sobrenome", models.CharField(max_length=100)),
+                ("first_name", models.CharField(max_length=100)),
+                ("last_name", models.CharField(max_length=100)),
                 ("cpf", models.CharField(max_length=11, unique=True)),
                 ("email", models.EmailField(max_length=254, unique=True)),
             ],
         ),
         migrations.CreateModel(
-            name="Endereco",
+            name="Address",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('rua', models.CharField(max_length=200)),
-                ('numero', models.CharField(max_length=10)),
-                ('bairro', models.CharField(max_length=100)),
-                ('cidade', models.CharField(max_length=100)),
-                ('estado', models.CharField(max_length=2)),
-                ('cep', models.CharField(max_length=9)),
-                ('usuario', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='enderecos', to='api_people.usuario')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("street", models.CharField(max_length=200)),
+                ("number", models.CharField(max_length=10)),
+                ("neighborhood", models.CharField(max_length=100)),
+                ("city", models.CharField(max_length=100)),
+                ("state", models.CharField(max_length=2)),
+                ("zip_code", models.CharField(max_length=9)),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="addresses",
+                        to="api_people.user",
+                    ),
+                ),
             ],
         ),
     ]
--- api_people/models.py
+++ api_people/models.py
@@ -1,26 +1,25 @@
+# app_name/models.py
 from django.db import models
 
 
-class Usuario(models.Model):
-    nome = models.CharField(max_length=100)
-    sobrenome = models.CharField(max_length=100)
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11, unique=True)
     email = models.EmailField(unique=True)
 
     def __str__(self):
-        return f"{self.nome} {self.sobrenome}"
+        return f"{self.first_name} {self.last_name}"
 
 
-class Endereco(models.Model):
-    usuario = models.ForeignKey(
-        Usuario, on_delete=models.CASCADE, related_name="enderecos"
-    )
-    rua = models.CharField(max_length=200)
-    numero = models.CharField(max_length=10)
-    bairro = models.CharField(max_length=100)
-    cidade = models.CharField(max_length=100)
-    estado = models.CharField(max_length=2)
-    cep = models.CharField(max_length=9)
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="addresses")
+    street = models.CharField(max_length=200)
+    number = models.CharField(max_length=10)
+    neighborhood = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=9)
 
     def __str__(self):
-        return f"{self.rua}, {self.numero} - {self.cidade}/{self.estado}"
+        return f"{self.street}, {self.number} - {self.city}/{self.state}"
